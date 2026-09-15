857aaee63e0ad1feaeda642ad5256a9780253c03
Tonmoy Biswas <tonmoybiswas13900@gmail.com>
2026-09-16 00:13:37 +0600

    added vowel and consonant program

--- mid_lab_test/problem-7.c
+++ mid_lab_test/problem-7.c
@@ -1,1 +1,17 @@
-// 
+// vowel and consonant
+#include <stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter a Alphabate:");
+    scanf("%c", &ch);
+    if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' || ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+    {
+        printf("Vowel\n");
+    }
+    else
+    {
+        printf("Consonant\n");
+    }
+    return 0;
+}
